6e085880ed1ce86c2061504191f5da2ff9aef5e3
Diego Mero <diegomero_1999@hotmail.com>
2023-02-10 02:00:57 -0500

    Add general shop list views

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -26,6 +26,20 @@ class FoodsController < ApplicationController
     redirect_to foods_url
   end
 
+  def general_shopping_list
+    @user_recipes = current_user.recipes
+    @missing_foods_cost = 0
+    @missing_recipe_foods = []
+    @user_recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        @food = Food.find(recipe_food.food_id)
+        @missing_foods_cost += recipe_food.quantity * @food.price if @food.user_id != current_user.id
+        @missing_food = recipe_food.food unless current_user.foods.include?(recipe_food.food)
+        @missing_recipe_foods << [recipe_food, @missing_food] if @missing_food
+      end
+    end
+  end
+
   private
 
   def form_params
